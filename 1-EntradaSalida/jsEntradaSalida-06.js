4ec6ed04294e2224b5b82df29949fb17e926c96b
Matias <matielian7l@gmail.com>
2022-04-28 00:32:09 -0300

    tarea realizada

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,6 +4,28 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+	var numeroUno;
+	var numeroDos;
+	var suma;
+
+	numeroUno = txtIdNumeroUno.value;
+
+	numeroUno = parseInt (numeroUno) ;
+
+
+	numeroDos = txtIdNumeroDos.value;
+	numeroDos= parseInt(numeroDos);
+	
+	
+
+	suma = numeroUno + numeroDos;
+
+	alert("La suma es : " + suma );
+
+
 }
 
+
+//txtIdNumeroUno
+
+//txtIdNumeroDos
--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -5,21 +5,74 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	alert("ok");	
+var numeroUno ;
+var numeroDos;							
+var suma ;
+
+ numeroUno = txtIdNumeroUno.value;
+ numeroUno = parseInt(numeroUno)
+ numeroDos = txtIdNumeroDos.value;
+ numeroDos = parseInt(numeroDos);
+ suma= (numeroUno) + (numeroDos);
+
+ alert ("el resultado de la suma " + suma) ; 
+
+
+// id numero 1 txtIdNumeroUno
+// id numero 2 txtIdNumeroDos
+
+
 }
 
 function restar()
-{
+{var numeroUno ;
+	var numeroDos;							
+	var resta ;
 	
+	 numeroUno = txtIdNumeroUno.value;
+	 numeroUno = parseInt(numeroUno)
+	 numeroDos = txtIdNumeroDos.value;
+	 numeroDos = parseInt(numeroDos);
+	 resta= numeroUno - numeroDos;
+	
+	 alert ("el resultado de la resta " + resta) ; 
+	
+	
+	
+
+
 }
 
 function multiplicar()
-{ 
+{ var numeroUno ;
+	var numeroDos;							
+	var multiplicar ;
+	
+	 numeroUno = txtIdNumeroUno.value;
+	 numeroUno = parseInt(numeroUno)
+	 numeroDos = txtIdNumeroDos.value;
+	 numeroDos = parseInt(numeroDos);
+	 multiplicar= numeroUno * numeroDos;
+	
+	 alert ("el resultado de la multiplicacion " + multiplicar) ; 
+	
 	
 }
 
 function dividir()
-{
+
+{var numeroUno ;
+	var numeroDos;							
+	var dividir ;
+	
+	 numeroUno = txtIdNumeroUno.value;
+	 numeroUno = parseInt(numeroUno)
+	 numeroDos = txtIdNumeroDos.value;
+	 numeroDos = parseInt(numeroDos);
+	 dividir= numeroUno / numeroDos;
+	
+	 alert ("el resultado de la division " + dividir) ; 
+
 	
 }
 
